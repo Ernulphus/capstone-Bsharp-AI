4e6be5d483dc578e96abff5dce2858d26b46d4e8
Boaz Kaufman <boaz.kaufman97@myhunter.cuny.edu>
2022-05-03 18:40:56 +0000

    Trombone and Tuba images

---
+++ counts.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#Script to count the number of images we have
+
+echo "Brass"
+echo " "
+echo "Trumpet "
+ls music_instruments_images/Brass/Trumpet | wc -l
+echo "Tuba "
+ls music_instruments_images/Brass/Tuba | wc -l
+echo "Trombone "
+ls music_instruments_images/Brass/Trombone | wc -l
+echo "FrenchHorn "
+ls music_instruments_images/Brass/FrenchHorn | wc -l
