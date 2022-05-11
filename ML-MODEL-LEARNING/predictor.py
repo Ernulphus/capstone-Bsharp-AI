ce0d7e812fe033e50945c77cd8164441dab54914
Boaz Kaufman <boaz.kaufman97@myhunter.cuny.edu>
2022-05-11 15:38:59 +0000

    Lots of image scraping

--- ML-MODEL-LEARNING/predictor.py
+++ ML-MODEL-LEARNING/predictor.py
@@ -1,40 +1,4 @@
 import tensorflow as tf
-
-from tensorflow.keras import datasets, layers, models
-import tensorflow_datasets as tfds
-import matplotlib.pyplot as plt
-from keras.preprocessing.image import load_img
-import numpy as np
-from matplotlib import pyplot as plt
-from PIL import Image
-import pickle
-import sys
-
-model = pickle.load(open('model.pkl','rb'))
-
-file_path = sys.argv[1] 
-img = Image.open(file_path)
-newsize = (256,256)
-img = img.resize(newsize)
-
-
-
-
-prediction_img = tf.keras.utils.img_to_array(
-    img, data_format=None, dtype=None
-)
-
-pred_image = np.array(prediction_img)
-
-
-
-pred_image = np.expand_dims(pred_image, axis = 0)
-
-pred_image = pred_image/255.0
-
-
-print(model.predict_on_batch(pred_image)import tensorflow as tf
-
 from tensorflow.keras import datasets, layers, models
 import tensorflow_datasets as tfds
 import matplotlib.pyplot as plt
@@ -46,22 +10,18 @@ import pickle
 import sys
 
 instrumentList = ['FrenchHorn','Trombone','Trumpet','Tuba']
-model = pickle.load(open('model.pkl','rb'))
+model = pickle.load(open('/root/capstone-Bsharp-AI/ML-MODEL-LEARNING/model.pkl','rb'))
 
 file_path = sys.argv[1] 
 img = Image.open(file_path)
 newsize = (256,256)
 img = img.resize(newsize)
 
-
-
-
 prediction_img = tf.keras.utils.img_to_array(
     img, data_format=None, dtype=None
 )
 
 pred_image = np.array(prediction_img)
-
 
 
 pred_image = np.expand_dims(pred_image, axis = 0)
@@ -72,14 +32,10 @@ predict_arr = model.predict_on_batch(pred_image)
 
 predict_list = predict_arr[0].tolist()
 
-print(predict_list)
+#print(predict_list)
 
 max_value = max(predict_list)
 
 max_index = predict_list.index(max_value)
 
 print(instrumentList[max_index], end='')
-
-
-
-)
